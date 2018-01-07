ba6d9271e919707e1b3f15963816d25af026a986
Dayt0n <daytonhasty@gmail.com>
2018-01-06 23:01:54 -0600

    linux uses date command differently than macOS

--- record/run.sh
+++ record/run.sh
@@ -9,7 +9,7 @@
 counter=0
 cd $(dirname $0)
 currentDate=`date +%Y-%m-%d`
-oldDate=`date -v -10d +%Y-%m-%d`
+oldDate=`date --date="10 days ago" +%Y-%m-%d`
 if [ -f record.log]; then
 	if grep -Fxq $oldDate record.log; then
 		# only keep logs of past 10 days
